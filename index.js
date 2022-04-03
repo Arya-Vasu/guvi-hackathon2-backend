7e1da5ae1d34b94097db1b6062b96fed699e146d
vasu.arya97@gmail.com <vasu.arya97@gmail.com>
2022-04-03 08:40:51 +0530

    First push

---
+++ index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+// const MONGO_URL = "mongodb://localhost";
+const MONGO_URL = process.env.MONGO_URL;
+
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongo is connected!");
+  return client;
+}
+
+const client = await createConnection();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/movies", async function (req, res) {
+  const result = await client
+    .db("Ticket_Booking")
+    .collection("Movies")
+    .find({})
+    .toArray();
+  res.send(result);
+});
+
+app.get("/theatres", async function (req, res) {
+  const result = await client
+    .db("Ticket_Booking")
+    .collection("Theatres")
+    .find({})
+    .toArray();
+  res.send(result);
+});
+
+app.post("/add-movies", async function (req, res) {
+  const data = req.body;
+  const result = await client
+    .db("Ticket_Booking")
+    .collection("Movies")
+    .insertMany(data);
+  res.send(result);
+});
+
+app.post("/add-theatres", async function (req, res) {
+  const data = req.body;
+  const result = await client
+    .db("Ticket_Booking")
+    .collection("Theatres")
+    .insertMany(data);
+  res.send(result);
+});
+
+app.get("/movie/:id", async function (req, res) {
+  const { id } = req.params;
+  const result = await client
+    .db("Ticket_Booking")
+    .collection("Movies")
+    .findOne({ id: id });
+  res.send(result);
+});
+
+app.listen(PORT, () => console.log(`Server started in ${PORT}`));
